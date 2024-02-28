01d1b79ed93352ef5365347cf88e24d4c61ff642
Antonio Neto <antoniogfneto92@hotmail.com>
2024-02-27 23:26:13 -0300

    ssl

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oafermffofR9gNdQ5d7',
     issuer: 'https://dev-56346414.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
