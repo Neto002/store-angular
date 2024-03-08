c9d918a50db5c220c5f2e7332284b3b8a2a27a69
Antonio Neto <antoniogfneto92@hotmail.com>
2024-03-08 16:08:06 -0300

    testando workflow

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
   oidc: {
-    clientId: '0oafermffofR9gNdQ5d7',
-    issuer: 'https://dev-56346414.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
+    clientId: '${OKTA_CLIENT_ID}',
+    issuer: '${OKTA_ISSUER}',
+    redirectUri: '${OKTA_REDIRECT_URL}',
     scopes: ['openid', 'profile', 'email'],
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http:localhost:8080/api',
-  stripePublishableKey:
-    'pk_test_51OodOHAJv5ulnNr72csryxkwytYKUkHHmaVMAR547UfkcZI3jtnFiIymKwK7WtyLCeehGsZu6q0MpdZoS4dljFHc00642aj98z',
+  apiUrl: '${API_URL}',
+  stripePublishableKey: '${STRIPE_SECRET}',
 };
