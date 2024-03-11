ab7a1d25c21c4223f56073832aeb49c3af90a44c
Antonio Neto <antoniogfneto92@hotmail.com>
2024-03-11 12:01:45 -0300

    testando workflow

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
-declare var process: {
-  env: {
-    API_URL: string;
-    STRIPE_SECRET: string;
-    OKTA_CLIENT_ID: string;
-    OKTA_ISSUER: string;
-  };
-};
+// declare var process: {
+//   env: {
+//     API_URL: string;
+//     STRIPE_SECRET: string;
+//     OKTA_CLIENT_ID: string;
+//     OKTA_ISSUER: string;
+//   };
+// };
 
 export const environment = {
   production: false,
-  apiUrl: process.env.API_URL,
-  stripePublishableKey: process.env.STRIPE_SECRET,
+  apiUrl: process.env['API_URL'],
+  stripePublishableKey: process.env['STRIPE_SECRET'],
 };
