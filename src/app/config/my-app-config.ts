8d81e33feda9417c8b4f4825464443d4c2da2060
Antonio Neto <antoniogfneto92@hotmail.com>
2024-03-11 11:22:25 -0300

    testando workflow

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: process.env['OKTA_CLIENT_ID'],
-    issuer: process.env['OKTA_ISSUER'],
+    clientId: process.env.OKTA_CLIENT_ID,
+    issuer: process.env.OKTA_ISSUER,
     redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
